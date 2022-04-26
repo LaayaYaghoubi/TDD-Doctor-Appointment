1c13b5857b76ad8f0bccf940a4a44c708fd1c988
Laaya <layayaghoubi@gmail.com>
2022-04-26 12:33:29 +0430

    persistence and migration layer added to the project git commit -m

---
+++ Src/DoctorAppointment.Persistence.EF/Appointments/AppointmentEntityMaps.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using DoctorAppointment.Entities;
+using DoctorAppointment.Entities.Appointments;
+
+namespace DoctorAppointment.Persistence.EF.Appointments
+{
+    public class AppointmentEntityMaps : IEntityTypeConfiguration<Appointment>
+    {
+        public void Configure(EntityTypeBuilder<Appointment> _)
+        {
+            _.ToTable("Appointments");
+            _.HasKey(_ => _.Id);
+            _.Property(_ => _.Id)
+                .ValueGeneratedOnAdd();
+
+            _.Property(_ => _.Date)
+                .IsRequired();
+
+            _.HasOne(_ => _.Doctor)
+                .WithMany(_ => _.Appointments)
+                .HasForeignKey(_ => _.DoctorId);
+
+            _.HasOne(_ => _.Patient)
+                .WithMany(_ => _.Appointments)
+                .HasForeignKey(_ => _.PatientId);
+        }
+    }
+}
