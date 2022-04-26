7f1575f08bfe27df97b7c102419f3f7977b004f6
Laaya <layayaghoubi@gmail.com>
2022-04-26 11:42:53 +0430

    TDD doctor appointment project doctor, patient, appointment entities added

---
+++ Src/DoctorAppointment.Entities/Doctors/Doctor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DoctorAppointment.Entities.Appointments;
+using DoctorAppointment.Entities.Patients;
+using DoctorAppointment.Entities.Persons;
+
+namespace DoctorAppointment.Entities.Doctors
+{
+    public class Doctor : Person
+    {
+
+        public Doctor()
+        {
+            Appointments = new HashSet<Appointment> { };
+        }
+           
+         
+        public string Field { get; set; }
+        public HashSet<Appointment> Appointments { get; set; }
+        
+    }
+}
---
+++ Src/DoctorAppointment.Entities/Patients/Patient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DoctorAppointment.Entities.Appointments;
+using DoctorAppointment.Entities.Persons;
+
+namespace DoctorAppointment.Entities.Patients
+{
+    public class Patient : Person
+    {
+        public Patient()
+        {
+            Appointments = new HashSet<Appointment> {};
+        }
+
+        public HashSet<Appointment> Appointments { get; set; }   
+    }
+}
