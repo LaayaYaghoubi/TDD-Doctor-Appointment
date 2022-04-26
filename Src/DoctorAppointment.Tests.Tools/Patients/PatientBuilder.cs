38f1ab637ff29fc95f2ced4395ccf734a4f63e6b
Laaya <layayaghoubi@gmail.com>
2022-04-26 16:58:02 +0430

    patient builder added to project

---
+++ Src/DoctorAppointment.Tests.Tools/Patients/PatientBuilder.cs
@@ -0,0 +1,40 @@
+﻿using DoctorAppointment.Entities.Patients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorAppointment.Tests.Tools.Patients
+{
+    public class PatientBuilder
+    {
+        Patient patient = new Patient();
+
+        public PatientBuilder()
+        {
+            patient = new Patient()
+            {
+                FirstName = "lili",
+                LastName = "potter",
+                NationalCode = "33467",
+            };
+        }
+            public PatientBuilder WithFirstName(string firstName)
+            {
+                patient.FirstName = firstName;
+                return this;
+            }
+           public PatientBuilder WithLastName(string lastName)
+            {
+            patient.LastName = lastName;
+            return this;
+            }
+            public PatientBuilder WithNationalCode(string nationalCode)
+            {
+            patient.NationalCode = nationalCode;
+            return this;
+            }
+    }
+    }
+
