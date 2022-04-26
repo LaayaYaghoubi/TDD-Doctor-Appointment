4034169e0b2cbc3807dba2cfcfaaa3a38db124f8
Laaya <layayaghoubi@gmail.com>
2022-04-26 17:17:20 +0430

    Appointment builder added to project

--- Src/DoctorAppointment.Tests.Tools/Appointments/AppintmentBuilder.cs
+++ Src/DoctorAppointment.Tests.Tools/Appointments/AppintmentBuilder.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DoctorAppointment.Entities.Appointments;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,33 @@ using System.Threading.Tasks;
 
 namespace DoctorAppointment.Tests.Tools.Appointments
 {
-    internal class AppintmentBuilder
+    public class AppintmentBuilder
     {
+        Appointment appointment = new Appointment();
+        public AppintmentBuilder()
+        {
+            appointment = new Appointment()
+            {
+                PatientId = 1,
+                DoctorId = 1,
+                Date = DateTime.Now,
+            };
+        }
+        public AppintmentBuilder WithPatientId(int id)
+        {
+            appointment.PatientId = id;
+            return this;
+        }
+        public AppintmentBuilder WithDoctorId(int id)
+        {
+            appointment.DoctorId = id;
+            return this;
+        }
+        public AppintmentBuilder WithDate(DateTime date)
+        {
+            appointment.Date = date;
+            return this;
+        }
+
     }
-}
+    }
